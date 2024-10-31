d68257fad08e6e754390d90cef134ed455c525cb
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-10-31 05:51:20 -0300

    feat: add assets folder and use in hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,32 +15,46 @@ export function Hero() {
   const { setVisibleSection } = useVisibleSectionStore();
   const ref = useRef(null);
 
-  // const { isScrollingDown } = useScrollDirection();
   const { isVisible } = useIsVisible(ref);
   isVisible && setVisibleSection(SECTIONS.HERO);
-
-  // useScrollToNextElement(SECTIONS.WHAT_I_DO, isScrollingDown, isVisible);
 
   return (
     <section
       ref={ref}
       id={SECTIONS.HERO}
-      className="px-default w-full flex items-center justify-center md:h-screen"
+      className="px-default w-full flex items-center justify-center md:h-screen relative"
     >
+      <Image
+        className="hidden lg:block absolute top-28"
+        src="images/assets/triangle.svg"
+        width={200}
+        height={210}
+        alt="Triangle svg"
+      />
       <div className="flex flex-col gap-8 pb-8 w-full max-w-default">
-        <div className="flex flex-col gap-16 md:flex-row md:items-center md:justify-between">
+        <div className="flex flex-col gap-16 md:gap-0 md:flex-row md:items-center md:justify-between">
           <div className="flex flex-col gap-3 max-w-[500px]">
             <Typography.H1 className="text-primary lg:text-7xl">
               Frontend Developer.
             </Typography.H1>
             <Typography.P className="text-xl">{t("SUBTITLE")}</Typography.P>
           </div>
-          <Image
-            src="/images/profile.png"
-            width={288}
-            height={460}
-            alt="Carlos Silva Profile Image"
-          />
+          <div className="relative w-fit">
+            <Image
+              className="z-10 relative w-[288px] h-[460px]"
+              src="/images/profile.png"
+              width={288}
+              height={460}
+              alt="Carlos Silva Profile Image"
+            />
+            <Image
+              className="z-0 absolute w-[288px] h-[460px] -bottom-4 -right-4 2xl:-bottom-8 2xl:-right-8"
+              src="images/assets/tall-rectangle.svg"
+              width={288}
+              height={460}
+              alt="Rectangle svg"
+            />
+          </div>
         </div>
 
         <div className="flex flex-col gap-3 md:flex-row md:gap-16">
@@ -52,6 +66,22 @@ export function Hero() {
           </Typography.P>
         </div>
       </div>
+
+      <Image
+        className="hidden md:block absolute -bottom-32 left-52"
+        src="images/assets/x.svg"
+        width={200}
+        height={232}
+        alt="X svg"
+      />
+
+      <Image
+        className="hidden lg:block absolute bottom-0 right-52"
+        src="images/assets/waves.svg"
+        width={300}
+        height={210}
+        alt="Waves svg"
+      />
     </section>
   );
 }
