61c68028a86a65c9131b37bb549d312ff282a028
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-12-01 17:32:26 -0300

    fix: add forwardRef to TransitionLink

--- src/components/ui/TransitionLink/index.tsx
+++ src/components/ui/TransitionLink/index.tsx
@@ -2,46 +2,53 @@
 
 import { sleep } from "@/constants";
 import { Link, Pages, usePathname, useRouter } from "@/navigation";
-import { ComponentProps } from "react";
+import { ComponentProps, ElementRef, forwardRef } from "react";
 
 type Props = ComponentProps<typeof Link> & {
   href: Pages;
   forceTransition?: boolean;
 };
-export function TransitionLink({
-  children,
-  href,
-  locale,
-  lang,
-  forceTransition = false,
-  onClick,
-  ...props
-}: Props) {
-  const pathname = usePathname();
-  const router = useRouter();
+export const TransitionLink = forwardRef<ElementRef<typeof Link>, Props>(
+  (
+    {
+      children,
+      href,
+      locale,
+      lang,
+      forceTransition = false,
+      onClick,
+      ...props
+    },
+    ref
+  ) => {
+    const pathname = usePathname();
+    const router = useRouter();
 
-  const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement>) => {
-    if (pathname === href && !forceTransition) return;
+    const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+      if (pathname === href && !forceTransition) return;
 
-    e.preventDefault();
-    const main = document.querySelector("main");
-    main?.classList.add("page-transition");
-    await sleep(200);
-    router.push(href, { locale });
-  };
+      e.preventDefault();
+      const main = document.querySelector("main");
+      main?.classList.add("page-transition");
+      await sleep(200);
+      router.push(href, { locale });
+    };
 
-  return (
-    <Link
-      onClick={(e) => {
-        onClick && onClick(e);
-        handleTransition(e);
-      }}
-      href={href}
-      locale={locale}
-      lang={lang}
-      {...props}
-    >
-      {children}
-    </Link>
-  );
-}
+    return (
+      <Link
+        onClick={(e) => {
+          onClick && onClick(e);
+          handleTransition(e);
+        }}
+        href={href}
+        locale={locale}
+        lang={lang}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Link>
+    );
+  }
+);
+TransitionLink.displayName = "TransitionLink";
