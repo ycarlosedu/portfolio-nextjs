d4f3bcae44342498e6b6614befa3f34190ebd53a
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-12-03 03:33:45 -0300

    feat: add scaling transition to carousel

--- src/components/ui/Carousel/index.tsx
+++ src/components/ui/Carousel/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 import Image from "next/image";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
-import { EmblaCarouselType } from "embla-carousel";
+import { EmblaCarouselType, EmblaEventType } from "embla-carousel";
 import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
 
 import { cn } from "@/lib/utils";
 import { DotButton, useDotButton } from "./dots";
+
+const TWEEN_FACTOR_BASE = 0.52;
+
+const numberWithinRange = (number: number, min: number, max: number): number =>
+  Math.min(Math.max(number, min), max);
 
 export type CarouselList = {
   name: string;
@@ -28,6 +33,8 @@ export default function Carousel({
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
     Autoplay({ delay: 3000 })
   ]);
+  const tweenFactor = useRef(0);
+  const tweenNodes = useRef<HTMLElement[]>([]);
 
   const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
     const autoplay = emblaApi?.plugins()?.autoplay;
@@ -46,24 +53,92 @@ export default function Carousel({
     onNavButtonClick
   );
 
+  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
+    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
+      return slideNode.querySelector(".embla__slide__content") as HTMLElement;
+    });
+  }, []);
+
+  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  }, []);
+
+  const tweenScale = useCallback(
+    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+      const engine = emblaApi.internalEngine();
+      const scrollProgress = emblaApi.scrollProgress();
+      const slidesInView = emblaApi.slidesInView();
+      const isScrollEvent = eventName === "scroll";
+
+      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+        let diffToTarget = scrollSnap - scrollProgress;
+        const slidesInSnap = engine.slideRegistry[snapIndex];
+
+        slidesInSnap.forEach((slideIndex) => {
+          if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+          if (engine.options.loop) {
+            engine.slideLooper.loopPoints.forEach((loopItem) => {
+              const target = loopItem.target();
+
+              if (slideIndex === loopItem.index && target !== 0) {
+                const sign = Math.sign(target);
+
+                if (sign === -1) {
+                  diffToTarget = scrollSnap - (1 + scrollProgress);
+                }
+                if (sign === 1) {
+                  diffToTarget = scrollSnap + (1 - scrollProgress);
+                }
+              }
+            });
+          }
+
+          const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current);
+          const scale = numberWithinRange(tweenValue, 0, 1).toString();
+          const tweenNode = tweenNodes.current[slideIndex];
+          if (tweenNode) tweenNode.style.transform = `scale(${scale})`;
+        });
+      });
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    setTweenNodes(emblaApi);
+    setTweenFactor(emblaApi);
+    tweenScale(emblaApi);
+
+    emblaApi
+      .on("reInit", setTweenNodes)
+      .on("reInit", setTweenFactor)
+      .on("reInit", tweenScale)
+      .on("scroll", tweenScale)
+      .on("slideFocus", tweenScale);
+  }, [emblaApi, setTweenFactor, setTweenNodes, tweenScale]);
+
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {list.map((item) => (
             <div key={item.name} className="embla__slide">
-              {showImageDescription && (
-                <span className="text-center top-1 text-sm text-black dark:text-white bg-white/80 dark:bg-black/80 p-1 absolute rounded-lg">
-                  {item.name}
-                </span>
-              )}
-              <Image
-                src={item.image}
-                alt={`Logo/Imagem do(a) ${item.name}`}
-                width={1244}
-                height={700}
-                className={cn(imageClassName)}
-              />
+              <div className="embla__slide__content">
+                {showImageDescription && (
+                  <span className="text-center z-10 top-1 text-sm text-black dark:text-white bg-white/80 dark:bg-black/80 p-1 absolute rounded-lg">
+                    {item.name}
+                  </span>
+                )}
+                <Image
+                  src={item.image}
+                  alt={`Logo/Imagem do(a) ${item.name}`}
+                  width={1244}
+                  height={700}
+                  className={cn(imageClassName, "embla__slide__image")}
+                />
+              </div>
             </div>
           ))}
         </div>
